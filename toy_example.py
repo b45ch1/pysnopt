6502e1b954d85543a360010de5d4ac2fff5e0b66
Manuel Kudruss <manuel.kudruss@iwr.uni-heidelberg.de>
2013-10-29 15:04:49 +0100

    adapted toy example main

--- toy_example.py
+++ toy_example.py
@@ -258,223 +258,203 @@ def char(len):
     return np.zeros((length), dtype=np.int8)
 
 def main():
-  lenrw = 20000
-  leniw = 10000
-  lencw = 500
+    lenrw = 20000
+    leniw = 10000
+    lencw = 500
 
-  minrw = np.zeros((1), dtype=np.float64)
-  miniw = np.zeros((1), dtype=np.int64)
-  mincw = np.zeros((1), dtype=np.int8)
+    minrw = np.zeros((1), dtype=np.float64)
+    miniw = np.zeros((1), dtype=np.int64)
+    mincw = np.zeros((1), dtype=np.int8)
 
-  rw = np.zeros((20000,), dtype=np.float64)
-  iw = np.zeros((10000,), dtype=np.int64)
-  cw = np.zeros((8*500,), dtype=np.int8)
+    rw = np.zeros((20000,), dtype=np.float64)
+    iw = np.zeros((10000,), dtype=np.int64)
+    cw = np.zeros((8*500,), dtype=np.int8)
 
-  Cold  = 0
-  Basis = 1
-  Warm  = 2
+    Cold  = 0
+    Basis = 1
+    Warm  = 2
 
-  x    = np.zeros((2,), dtype=np.float64)
-  xlow = np.zeros((2,), dtype=np.float64)
-  xupp = np.zeros((2,), dtype=np.float64)
-  xmul = np.zeros((2,), dtype=np.float64)
-  F    = np.zeros((3,), dtype=np.float64)
-  Flow = np.zeros((3,), dtype=np.float64)
-  Fupp = np.zeros((3,), dtype=np.float64)
-  Fmul = np.zeros((3,), dtype=np.float64)
+    x    = np.zeros((2,), dtype=np.float64)
+    xlow = np.zeros((2,), dtype=np.float64)
+    xupp = np.zeros((2,), dtype=np.float64)
+    xmul = np.zeros((2,), dtype=np.float64)
+    F    = np.zeros((3,), dtype=np.float64)
+    Flow = np.zeros((3,), dtype=np.float64)
+    Fupp = np.zeros((3,), dtype=np.float64)
+    Fmul = np.zeros((3,), dtype=np.float64)
 
-  ObjAdd = np.zeros((1,), dtype=np.float64)
+    ObjAdd = np.zeros((1,), dtype=np.float64)
 
-  xstate = np.zeros((2,), dtype=np.int64)
-  Fstate = np.zeros((3,), dtype=np.int64)
+    xstate = np.zeros((2,), dtype=np.int64)
+    Fstate = np.zeros((3,), dtype=np.int64)
 
-  INFO   = np.zeros((1,), dtype=np.int64)
-  ObjRow = np.zeros((1,), dtype=np.int64)
-  n      = np.zeros((1,), dtype=np.int64)
-  neF    = np.zeros((1,), dtype=np.int64)
+    INFO   = np.zeros((1,), dtype=np.int64)
+    ObjRow = np.zeros((1,), dtype=np.int64)
+    n      = np.zeros((1,), dtype=np.int64)
+    neF    = np.zeros((1,), dtype=np.int64)
 
-  lenA   = np.zeros((1,), dtype=np.int64)
-  lenA[0] = 10
+    lenA   = np.zeros((1,), dtype=np.int64)
+    lenA[0] = 10
 
-  iAfun = np.zeros((10,), dtype=np.int64)
-  jAvar = np.zeros((10,), dtype=np.int64)
+    iAfun = np.zeros((10,), dtype=np.int64)
+    jAvar = np.zeros((10,), dtype=np.int64)
 
-  A     = np.zeros((1,), dtype=np.float64)
+    A     = np.zeros((1,), dtype=np.float64)
 
-  lenG   = np.zeros((1,), dtype=np.int64)
-  lenG[0] = 10
+    lenG   = np.zeros((1,), dtype=np.int64)
+    lenG[0] = 10
 
-  iGfun = np.zeros((10,), dtype=np.int64)
-  jGvar = np.zeros((10,), dtype=np.int64)
+    iGfun = np.zeros((10,), dtype=np.int64)
+    jGvar = np.zeros((10,), dtype=np.int64)
 
-  neA = np.zeros((1,), dtype=np.int64)
-  neG = np.zeros((1,), dtype=np.int64)
+    neA = np.zeros((1,), dtype=np.int64)
+    neG = np.zeros((1,), dtype=np.int64)
 
-  nxname    = np.zeros((1,), dtype=np.int64)
-  nFname    = np.zeros((1,), dtype=np.int64)
-  npname = np.zeros((1,), dtype=np.int64)
+    nxname    = np.zeros((1,), dtype=np.int64)
+    nFname    = np.zeros((1,), dtype=np.int64)
+    npname = np.zeros((1,), dtype=np.int64)
 
-  nxname[0] = 1
-  nFname[0] = 1
+    nxname[0] = 1
+    nFname[0] = 1
 
-  xnames = np.zeros((1*8,), dtype=np.int8)
-  Fnames = np.zeros((1*8,), dtype=np.int8)
-  Prob   = np.zeros((200*8,), dtype=np.int8)
+    xnames = np.zeros((1*8,), dtype=np.int8)
+    Fnames = np.zeros((1*8,), dtype=np.int8)
+    Prob   = np.zeros((200*8,), dtype=np.int8)
 
-  iSpecs   = np.zeros((1,), dtype=np.int64)
-  spec_len = np.zeros((1,), dtype=np.int64)
-  prnt_len = np.zeros((1,), dtype=np.int64)
-  iSumm    = np.zeros((1,), dtype=np.int64)
-  iPrint   = np.zeros((1,), dtype=np.int64)
+    iSpecs   = np.zeros((1,), dtype=np.int64)
+    spec_len = np.zeros((1,), dtype=np.int64)
+    prnt_len = np.zeros((1,), dtype=np.int64)
+    iSumm    = np.zeros((1,), dtype=np.int64)
+    iPrint   = np.zeros((1,), dtype=np.int64)
 
-  iSpecs[0] = 4
-  iSumm [0] = 6
-  iPrint[0] = 9
+    iSpecs[0] = 4
+    iSumm [0] = 6
+    iPrint[0] = 9
 
-  printname = np.zeros((200*8,), dtype=np.int8)
-  specname  = np.zeros((200*8,), dtype=np.int8)
+    printname = np.zeros((200*8,), dtype=np.int8)
+    specname  = np.zeros((200*8,), dtype=np.int8)
 
-  nS   = np.zeros((1,), dtype=np.int64)
-  nInf = np.zeros((1,), dtype=np.int64)
+    nS   = np.zeros((1,), dtype=np.int64)
+    nInf = np.zeros((1,), dtype=np.int64)
 
-  sInf = np.zeros((1,), dtype=np.float64)
+    sInf = np.zeros((1,), dtype=np.float64)
 
-  DerOpt     = np.zeros((1,), dtype=np.int64)
-  Major      = np.zeros((1,), dtype=np.int64)
-  iSum       = np.zeros((1,), dtype=np.int64)
-  iPrt       = np.zeros((1,), dtype=np.int64)
-  strOpt_len = np.zeros((1,), dtype=np.int64)
-  strOpt     = np.zeros((200*8,), dtype=np.int8)
+    DerOpt     = np.zeros((1,), dtype=np.int64)
+    Major      = np.zeros((1,), dtype=np.int64)
+    iSum       = np.zeros((1,), dtype=np.int64)
+    iPrt       = np.zeros((1,), dtype=np.int64)
+    strOpt_len = np.zeros((1,), dtype=np.int64)
+    strOpt     = np.zeros((200*8,), dtype=np.int8)
 
-  print("\nSolving toy0 without first derivatives ...\n")
+    print("\nSolving toy0 without first derivatives ...\n")
 
-  # open output files using snfilewrappers.[ch] */
-  #sprintf(specname ,   "%s", "sntoya.spc")   spec_len = strlen(specname)
-  #sprintf(printname,   "%s", "sntoya.out")   prnt_len = strlen(printname)
+    # open output files using snfilewrappers.[ch] */
+    specn  = "sntoya.spc"
+    printn = "sntoya.out"
+    specname [:len(specn)  = [ord(x) for x in specn]
+    printname[:len(printn) = [ord(x) for x in printn]
 
-  # Open the print file, fortran style */
-  #snopenappend_
-  #  ( &iPrint, printname,   &INFO, prnt_len )
+    # Open the print file, fortran style */
+    snopt.snopenappend(iPrint, printname, INFO)
 
-  #     ================================================================== */
-  #     First,  sninit_ MUST be called to initialize optional parameters   */
-  #     to their default values.                                           */
-  #     ================================================================== */
+    # ================================================================== */
+    # First,  sninit_ MUST be called to initialize optional parameters   */
+    # to their default values.                                           */
+    # ================================================================== */
 
-  sninit_
-    ( &iPrint, &iSumm, cw, &lencw, iw, &leniw, rw, &lenrw, 8*500 )
+    snopt.sninit(iPrint, iSumm, cw, iw, rw)
 
-  #     Set up the problem to be solved.                       */
-  #     No derivatives are set in this case.                   */
-  #     NOTE: To mesh with Fortran style coding,               */
-  #           it ObjRow must be treated as if array F          */
-  #           started at 1, not 0.  Hence, if F(0) = objective */
-  #           then ObjRow should be set to 1.                  */
+    # Set up the problem to be solved.                       */
+    # No derivatives are set in this case.                   */
+    # NOTE: To mesh with Fortran style coding,               */
+    #       it ObjRow must be treated as if array F          */
+    #       started at 1, not 0.  Hence, if F(0) = objective */
+    #       then ObjRow should be set to 1.                  */
 
-  toy0
-    ( &INFO, Prob, &neF, &n, &ObjAdd, &ObjRow, xlow, xupp,
-      Flow, Fupp, x, xstate, Fmul )
-  npname = strlen(Prob)
+    toy0(INFO, Prob, neF, n, ObjAdd, ObjRow, xlow, xupp,
+         Flow, Fupp, x, xstate, Fmul)
+    npname = len(Prob)
 
-  #     SnoptA will compute the Jacobian by finite-differences.   */
-  #     The user has the option of calling  snJac  to define the  */
-  #     coordinate arrays (iAfun,jAvar,A) and (iGfun, jGvar).     */
+    # SnoptA will compute the Jacobian by finite-differences.   */
+    # The user has the option of calling  snJac  to define the  */
+    # coordinate arrays (iAfun,jAvar,A) and (iGfun, jGvar).     */
 
-  snjac_
-    ( &INFO, &neF, &n, toyusrf_,
-      iAfun, jAvar, &lenA, &neA, A,
-      iGfun, jGvar, &lenG, &neG,
-      x, xlow, xupp, &mincw, &miniw, &minrw,
-      cw, &lencw, iw, &leniw, rw, &lenrw,
-      cw, &lencw, iw, &leniw, rw, &lenrw,
-      8*500, 8*500 )
+    snopt.snjac(INFO, neF, n, toyusrf_,
+                iAfun, jAvar, lenA, neA, A,
+                iGfun, jGvar, lenG, neG,
+                x, xlow, xupp, mincw, miniw, minrw,
+                cw, iw, rw, cw, iw, rw)
 
-  #     ------------------------------------------------------------------ */
-  #     Warn SnoptA that userf does not compute derivatives.               */
-  #     The parameters iPrt and iSum may refer to the Print and Summary    */
-  #     file respectively.  Setting them to 0 suppresses printing.         */
-  #     ------------------------------------------------------------------ */
+    # ------------------------------------------------------------------ */
+    # Warn SnoptA that userf does not compute derivatives.               */
+    # The parameters iPrt and iSum may refer to the Print and Summary    */
+    # file respectively.  Setting them to 0 suppresses printing.         */
+    # ------------------------------------------------------------------ */
 
-  DerOpt = 0
-  iPrt   = 0
-  iSum   = 0
-  sprintf(strOpt,"%s","Derivative option")
-  strOpt_len = strlen(strOpt)
-  snseti_
-    ( strOpt, &DerOpt, &iPrt, &iSum, &INFO,
-      cw, &lencw, iw, &leniw, rw, &lenrw, strOpt_len, 8*500 )
+    DerOpt = 0
+    iPrt   = 0
+    iSum   = 0
+    strOpt_s = "Derivative option"
+    strOpt[:len(strOpt] = [ord(x) for x in strOpt_s]
+    strOpt_len = len(strOpt)
+    snopt.snseti(strOpt, DerOpt, iPrt, iSum, INFO, cw, iw, rw)
 
-  #     ------------------------------------------------------------------ */
-  #     Go for it, using a Cold start.                                     */
-  #     ------------------------------------------------------------------ */
+    #     ------------------------------------------------------------------ */
+    #     Go for it, using a Cold start.                                     */
+    #     ------------------------------------------------------------------ */
 
-  snopta_
-    ( &Cold, &neF, &n, &nxname, &nFname,
-      &ObjAdd, &ObjRow, Prob, toyusrf_,
-      iAfun, jAvar, &lenA, &neA, A,
-      iGfun, jGvar, &lenG, &neG,
-      xlow, xupp, xnames, Flow, Fupp, Fnames,
-      x, xstate, xmul, F, Fstate, Fmul,
-      &INFO, &mincw, &miniw, &minrw,
-      &nS, &nInf, &sInf,
-      cw, &lencw, iw, &leniw, rw, &lenrw,
-      cw, &lencw, iw, &leniw, rw, &lenrw,
-      npname, 8*nxname, 8*nFname,
-      8*500, 8*500)
+    snopta(Cold, neF, n, nxname, nFname,
+           ObjAdd, ObjRow, Prob, toyusrf_,
+           iAfun, jAvar, lenA, neA, A,
+           iGfun, jGvar, lenG, neG,
+           xlow, xupp, xnames, Flow, Fupp, Fnames,
+           x, xstate, xmul, F, Fstate, Fmul,
+           INFO, mincw, miniw, minrw,
+           nS, nInf, sInf, cw,  iw,  rw, cw,  iw,  rw )
 
+    print("\nSolving toy1 using first derivatives ...\n")
 
-  printf("\nSolving toy1 using first derivatives ...\n")
+    toy1(INFO, Prob, neF, n,
+         iAfun, jAvar, lenA, neA, A,
+         iGfun, jGvar, lenG, neG,
+         ObjAdd, ObjRow, xlow, xupp,
+         Flow, Fupp, x, xstate, Fmul)
 
-  toy1
-    ( &INFO, Prob, &neF, &n,
-      iAfun, jAvar, &lenA, &neA, A,
-      iGfun, jGvar, &lenG, &neG,
-      &ObjAdd, &ObjRow, xlow, xupp,
-      Flow, Fupp, x, xstate, Fmul )
+    # Read in specs file (optional) */
+    # snfilewrapper_ will open the specs file, fortran style, */
+    # then call snspec_ to read in specs.                        */
 
-  # Read in specs file (optional) */
-  # snfilewrapper_ will open the specs file, fortran style, */
-  # then call snspec_ to read in specs.                        */
+    snopt.snfilewrapper(specname, iSpecs, INFO, cw, iw, rw)
 
-  snfilewrapper_
-    ( specname, &iSpecs, &INFO, cw, &lencw,
-      iw, &leniw, rw, &lenrw, spec_len, 8*lencw)
+    if INFO[0] != 101:
+        print("Warning: trouble reading specs file %s \n"%(specname))
 
-  if( INFO != 101 )
+    # Specify any user options not set in the Specs file. */
+    DerOpt = 1
+    snopt.snseti(strOpt, DerOpt, iPrint, iSumm, INFO, cw, iw, rw)
 
-      printf("Warning: trouble reading specs file %s \n", specname)
+    Major = 250
+    strOpt_s = "Major Iteration limit"
+    strOpt[:len(strOpt] = [ord(x) for x in strOpt_s]
+    strOpt_len = len(strOpt)
+    snopt.snseti( strOpt, Major, iPrint, iSumm, INFO, cw, iw, rw)
 
+    # ------------------------------------------------------------------ */
+    # Solve the problem again, this time with derivatives specified.     */
+    # ------------------------------------------------------------------ */
 
-  # Specify any user options not set in the Specs file. */
-  DerOpt = 1
-  snseti_
-    ( strOpt, &DerOpt, &iPrint, &iSumm, &INFO,
-      cw, &lencw, iw, &leniw, rw, &lenrw, strOpt_len, 8*500 )
+    snopta(Cold, neF, n, nxname, nFname,
+           ObjAdd, ObjRow, Prob, toyusrfg_,
+           iAfun, jAvar, lenA, neA, A,
+           iGfun, jGvar, lenG, neG,
+           xlow, xupp, xnames, Flow, Fupp, Fnames,
+           x, xstate, xmul, F, Fstate, Fmul,
+           INFO, mincw, miniw, minrw,
+           nS, nInf, sInf, cw, iw, rw, cw, iw, rw)
 
-  Major = 250
-  strcpy( strOpt,"Major Iteration limit")
-  strOpt_len = strlen(strOpt)
-  snseti_
-    ( strOpt, &Major, &iPrint, &iSumm, &INFO,
-      cw, &lencw, iw, &leniw, rw, &lenrw, strOpt_len, 8*500 )
+    snopt.snclose(iPrint)
+    snopt.snclose(iSpecs)
 
-  #     ------------------------------------------------------------------ */
-  #     Solve the problem again, this time with derivatives specified.     */
-  #     ------------------------------------------------------------------ */
-  snopta_
-    ( &Cold, &neF, &n, &nxname, &nFname,
-      &ObjAdd, &ObjRow, Prob, toyusrfg_,
-      iAfun, jAvar, &lenA, &neA, A,
-      iGfun, jGvar, &lenG, &neG,
-      xlow, xupp, xnames, Flow, Fupp, Fnames,
-      x, xstate, xmul, F, Fstate, Fmul,
-      &INFO, &mincw, &miniw, &minrw,
-      &nS, &nInf, &sInf,
-      cw, &lencw, iw, &leniw, rw, &lenrw,
-      cw, &lencw, iw, &leniw, rw, &lenrw,
-      npname, 8*nxname, 8*nFname,
-      8*500, 8*500)
-
-  snclose_( &iPrint )
-  snclose_( &iSpecs )
+if __name__ == '__main__':
+    main()
 
