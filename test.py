83a87b5d636626ab71754cd98f27d126e9d8c318
Sebastian Walter <sebastian.walter@iwr.uni-heidelberg.de>
2013-11-05 18:02:53 +0100

    update test.py to int32

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ iSumm  = -1
 
 
 cw = numpy.array([0]*500*8, dtype=numpy.character)
-iw = numpy.array([0]*500,   dtype=numpy.int64)
+iw = numpy.array([0]*500,   dtype=numpy.int32)
 rw = numpy.array([0]*500,   dtype=numpy.float64)
 cu = numpy.array([0]*500*8, dtype=numpy.character)
-iu = numpy.array([0]*500,   dtype=numpy.int64)
+iu = numpy.array([0]*500,   dtype=numpy.int32)
 ru = numpy.array([0]*500,   dtype=numpy.float64)
 
 
@@ -21,31 +21,31 @@ snopt.check_memory_compatibility()
 
 # snopt.sninit(iPrint, iSumm, cw, iw, rw)
 
-inform = numpy.array([0]*1,   dtype=numpy.int64)
-nf     = numpy.array([0]*1,   dtype=numpy.int64)
-n      = numpy.array([0]*1,   dtype=numpy.int64)
-iafun  = numpy.array([0]*1,   dtype=numpy.int64)
-javar  = numpy.array([0]*1,   dtype=numpy.int64)
-nea    = numpy.array([0]*1,   dtype=numpy.int64)
-lena   = numpy.array([0]*1,   dtype=numpy.int64)
+inform = numpy.array([0]*1,   dtype=numpy.int32)
+nf     = numpy.array([0]*1,   dtype=numpy.int32)
+n      = numpy.array([0]*1,   dtype=numpy.int32)
+iafun  = numpy.array([0]*1,   dtype=numpy.int32)
+javar  = numpy.array([0]*1,   dtype=numpy.int32)
+nea    = numpy.array([0]*1,   dtype=numpy.int32)
+lena   = numpy.array([0]*1,   dtype=numpy.int32)
 
-igfun  = numpy.array([0]*1,   dtype=numpy.int64)
-jgvar  = numpy.array([0]*1,   dtype=numpy.int64)
-neg    = numpy.array([0]*1,   dtype=numpy.int64)
-leng   = numpy.array([0]*1,   dtype=numpy.int64)
+igfun  = numpy.array([0]*1,   dtype=numpy.int32)
+jgvar  = numpy.array([0]*1,   dtype=numpy.int32)
+neg    = numpy.array([0]*1,   dtype=numpy.int32)
+leng   = numpy.array([0]*1,   dtype=numpy.int32)
 
 a      = numpy.array([0]*500,   dtype=numpy.float64)
 x      = numpy.array([0]*n[0],   dtype=numpy.float64)
 xlow   = numpy.array([0]*n[0],   dtype=numpy.float64)
 xupp   = numpy.array([0]*n[0],   dtype=numpy.float64)
 
-mincw  = numpy.array([0]*1,   dtype=numpy.int64)
-miniw  = numpy.array([0]*1,   dtype=numpy.int64)
-minrw  = numpy.array([0]*1,   dtype=numpy.int64)
+mincw  = numpy.array([0]*1,   dtype=numpy.int32)
+miniw  = numpy.array([0]*1,   dtype=numpy.int32)
+minrw  = numpy.array([0]*1,   dtype=numpy.int32)
 
 
-def userfun(status, x, needF, F, needG, G, cu, iu, ru):
-	print 'hallo'
+def userfun(status, x, needF, neF, F, needG, neG, G, cu, iu, ru):
+      print 'hallo'
 
 snopt.snjac( inform,
              nf,
